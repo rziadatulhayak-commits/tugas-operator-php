6877ebc61f08c7e60534c0f8889a5b18e96b235d
rziadatulhayak-commits <rziadatulhayak@gmail.com>
2026-05-06 20:23:02 +0800

    Add files via upload

---
+++ operator.php
@@ -0,0 +1,14 @@
+<?php
+//operator aritmatika
+$angka1 =10;
+$angka2= 7;
+$hasilpenjumlahan = $angka1 + $angka2; //penjumlahan
+$hasilpengurangan = $angka1 - $angka2; //pengurangan
+$hasilperkalian = $angka1 * $angka2; //perkalian
+$hasilpembagian = $angka1 / $angka2; //pembagian
+echo "Hasil penjumlahan: " .$angka1 . " + " . $angka2 . " = " .$hasilpenjumalhan . "<br>";
+echo "hasilpengurangan: " .$angka1 . " - " . $angka2 ." = " .$hasilpengurangan . "<br>";
+echo "hasilperkalian: " .$angka1 . " * " . $angka2 ." = " .$hasilperkalian . "<br>";
+
+
+>? 
